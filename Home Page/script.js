7441bdf2db7707d536511bf7d66cce593406f0d9
ami798 <amiraabdurahman8@gmail.com>
2025-03-21 16:00:05 +0300

    js

---
+++ Home Page/script.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('.hero-section h1', { delay: 500, distance: '50px', origin: 'left' });
+ScrollReveal().reveal('.hero-section .statistics', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.features-section .feature-item', {
+  interval: 200,
+  origin: 'bottom',
+});
+ScrollReveal().reveal('.about-us-section .section-title', { delay: 500, origin: 'top' });
+ScrollReveal().reveal('.about-us-section .cards .card', {
+  interval: 300,
+  origin: 'bottom',
+});
+ScrollReveal().reveal('.latest-programs-section h2', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.latest-programs-section button', { delay: 700, origin: 'right' });
